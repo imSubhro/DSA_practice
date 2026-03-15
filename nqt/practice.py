c5dcb62d4fc3efdc8e8cea675895422de5e032f8
imSubhro <subhromohanta16@gmail.com>
2026-03-16 02:12:06 +0530

    prep for nqt

---
+++ nqt/practice.py
@@ -0,0 +1,29 @@
+# prime Number
+# num = int(input("Enter a number: "))
+
+# if num <= 1:
+#     print("Not prime")
+# else:
+#     for i in range (2,num):
+#         if num%i ==0 :
+#             print("Not prime")
+#             break
+#     else:        
+#         print("Prime Number")
+
+
+#palindrome Number
+# num = int(input("Enter a number: "))
+
+# temp =num
+# rev = 0 
+
+# while temp > 0:
+#     digit = temp % 10
+#     rev = rev *10 + digit 
+#     temp = temp // 10
+
+# if num == rev:
+#     print("Palindrome Number")
+# else:
+#     print("Not a Palindrome Number")
