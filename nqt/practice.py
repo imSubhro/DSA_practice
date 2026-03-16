1ce4b4a06efde46a5f50f513b375f7629d676c86
imSubhro <subhromohanta16@gmail.com>
2026-03-16 20:29:53 +0530

    extra something

--- nqt/practice.py
+++ nqt/practice.py
@@ -27,3 +27,92 @@
 #     print("Palindrome Number")
 # else:
 #     print("Not a Palindrome Number")
+
+
+#reverse number
+
+# num = int(input("Enter a number: "))
+
+# temp = num 
+# rev= 0
+
+# while temp>0:
+#     digit = temp%10
+#     rev = rev*10 +digit
+#     temp = temp //10
+# print("Reverse of", num, "is: ", rev)
+
+#fibonacci Series
+# num = int(input("Enter number: "))
+
+# a=0
+# b=1
+
+# print("Fibonacci Series: ", end=" ")
+
+# for i in range(num):
+#     print(a, end=" ")
+#     c= a+b
+#     a=b
+#     b=c
+
+
+# #factorial of a number
+
+# num = int(input("Enter a number: "))
+
+# fact = 1
+
+# for i in range (1, num+1):
+#     fact = fact * i
+
+# print("Factorial of", num, "is: ", fact)
+
+
+#  Armstrong Number
+
+# num = int(input("Enter a number: "))
+
+# temp = num 
+# sum = 0
+# while temp > 0:
+#     digit = temp % 10
+#     sum = sum + digit ** 3
+#     temp = temp // 10
+
+# if num == sum:
+#     print(num, "is an Armstrong Number")
+# else:   print(num, "is not an Armstrong Number")        
+
+
+#leap Year
+
+# num = int(input("Enter a year: "))
+
+# if (num%4 ==0 and num%100 !=0) or (num%400 ==0):
+#     print(num, "is a leap year")
+# else:
+#     print(num, "is not a leap year")
+
+
+#lcm check
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# max_num = max(num1, num2)
+
+# while True:
+#     if max_num % num1 == 0 and max_num % num2 == 0:
+#         print("LCM of", num1, "and", num2, "is: ", max_num)
+#         break
+#     max_num += 1
+
+#gcd/hcf check
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+# if num2 != 0:
+#     num1, num2 = num2, num1 % num2
+# print("GCD/HCF of the two numbers is: ", num1)
